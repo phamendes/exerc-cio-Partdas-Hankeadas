b96fbe078c6f7a804770abad0c222b34fcb019b9
Phablo <miranda.phablo@live.com>
2024-10-16 06:50:45 -0300

    feito a estrutura inical com if else

---
+++ CalcDeRank.js
@@ -0,0 +1,19 @@
+
+function determinarNivel(vitorias) {
+    if (vitorias < 10) {
+        return "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        return "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        return "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        return "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        return "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        return "Lendário";
+    } else if (vitorias >= 101) {
+        return "Imortal";
+    }
+}
+
